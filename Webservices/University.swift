ed85df644fd2c314f754dc5987ae95e5385f54bf
HARSHDWIVEDiii <dubeyayush76073@gmail.com>
2024-01-04 18:44:48 +0530

    Initial Commit

---
+++ Webservices/University.swift
@@ -0,0 +1,23 @@
+//
+//  University.swift
+//  Webservices
+//
+//  Created by Mac on 04/01/24.
+//
+
+import Foundation
+struct University {
+    
+    var page : Int
+    var per_page : Int
+    var total : Int
+    var total_pages : Int
+    var data : [Datas]
+}
+struct Datas{
+    var id : Int
+    var first_name : String
+    var last_name : String
+    var avatar : String
+    var email : String
+}
---
+++ Webservices/UsersDetailCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  UsersDetailCollectionViewCell.swift
+//  Webservices
+//
+//  Created by Mac on 04/01/24.
+//
+
+import UIKit
+
+class UsersDetailCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var first_name: UILabel!
+    @IBOutlet weak var last_name: UILabel!
+    
+    @IBOutlet weak var userPageLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
